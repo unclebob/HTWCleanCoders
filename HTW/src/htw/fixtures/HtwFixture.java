b6d6b6e5afa9fb689d68149838aa175613666827
Robert C. Martin <unclebob@cleancoder.com>
2015-10-06 10:06:11 -0500

    initial commit

---
+++ HTW/src/htw/fixtures/HtwFixture.java
@@ -0,0 +1,23 @@
+package htw.fixtures;
+
+public class HtwFixture {
+  public boolean ConnectCavernToGoing(int c1, int c2, String dir) {
+    return false;
+  }
+
+  public boolean putPlayerInCavern(int c) {
+    return false;
+  }
+
+  public boolean movePlayer(String dir) {
+    return false;
+  }
+
+  public int getPlayerCavern() {
+    return -1;
+  }
+
+  public boolean ErrorIdWasRaised(String id) {
+    return false;
+  }
+}
